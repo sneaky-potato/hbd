ddfb5d7bb944df128a832943e3c85ee9d488656e
sneaky-potato <sneakypotato.hello@gmail.com>
2022-04-25 00:09:17 +0530

    GET serializer add

--- src/models/birthday.py
+++ src/models/birthday.py
@@ -1,4 +1,5 @@
 from src.models import db
+import calendar
 
 class Birthday(db.Model):
     __tablename__ = 'bdays'
@@ -7,3 +8,11 @@ class Birthday(db.Model):
     name = db.Column(db.String)
     bday_day = db.Column(db.Integer)
     bday_month = db.Column(db.Integer)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'birthday': str(self.bday_day) + ' ' + str(calendar.month_name[self.bday_month])
+        }
