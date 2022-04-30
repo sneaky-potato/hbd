872b66d1e4d9ad222e6797a6d187899dbef6605e
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-04-30 15:54:38 +0530

    add date check for post request, add search route

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from src.models.init_db import db
 from src.models.models import Birthday
+from calendar import monthrange
+from datetime import date
 
 route_blueprint = Blueprint('route_blueprint', __name__)
 
@@ -19,6 +21,11 @@ def post_birthday():
     data = request.get_json()
     try:
         name, bday_day, bday_month = data['name'], data['bday_day'], data['bday_month']
+
+        num_days = monthrange(date.today().year, bday_month)[1]
+        if(bday_day < 1 or bday_day > num_days):
+            raise ValueError
+
         birthday = Birthday(name=name, bday_day=bday_day, bday_month=bday_month)
         db.session.add(birthday)
         db.session.commit()
@@ -26,3 +33,31 @@ def post_birthday():
 
     except KeyError:
         return jsonify({'data': 'failure', 'details': 'check request body'}), 400
+    except ValueError:
+        return jsonify({'data': 'failure', 'details': 'check birthday day and month'}), 400
+    except:
+        return jsonify({'data': 'failure', 'details': 'something went wrong'}), 400
+
+@route_blueprint.route('/birthday/search', methods=['GET', 'POST'])
+def search_birthday():
+    try:
+        name = request.args.get('name')
+        bday_day = request.args.get('bday_day')
+        bday_month = request.args.get('bday_month')
+
+        if ((name is None) and (bday_day is None) and (bday_month is None)):
+            raise KeyError
+
+        birthdays = Birthday.query
+
+        if bday_month:
+            birthdays = birthdays.filter(Birthday.bday_month == bday_month)
+        if bday_day:
+            birthdays = birthdays.filter(Birthday.bday_day == bday_day)
+        if name:
+            birthdays = birthdays.filter(Birthday.name.ilike('%' + str(name) + '%')).all()
+            print(birthdays)
+        return jsonify({'data': [birthday.serialize for birthday in birthdays]}), 200
+    except:
+        return jsonify({'data': 'failure', 'details': 'check url params'}), 400
+
